d1c195725decdae623befbe108831252c3fdada4
rubi-lesmana <rubbyghozaly@gmail.com>
2026-01-13 22:10:04 +0700

    Tugas 1 Javascript Fundamental

---
+++ tugas1.js
@@ -0,0 +1,14 @@
+// Buatlah Program Dengan Spesifikasi dibawah ini :
+
+// Buatlah 3 buah variabel menggunakan var , let dan const
+// Variabel var berisikan nama depan teman teman , variabel let berisikan nama akhir teman teman , const berisikan umur teman teman saat ini
+// Setelah itu teman teman tampilkan ketiga variabel tersebut dengan console.log()
+// Hint : number tanpa petik string dengan petik
+
+var first_name = "Rubi";
+let last_name = "Lesmana";
+const age = 27;
+
+console.log("Nama depan saya adalah", first_name);
+console.log("Nama belakang saya adalah", last_name);
+console.log("Umur saya adalah", age);
