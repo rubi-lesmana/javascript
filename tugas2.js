2a08685606be23edea967c2a49472f32d10966ef
rubi-lesmana <rubbyghozaly@gmail.com>
2026-01-14 21:52:12 +0700

    Tugas 2 Javascript Fundamental

---
+++ tugas2.js
@@ -0,0 +1,23 @@
+// Tugas 2 tetnang tipe data
+
+// Tipe data Number
+var number = 30;
+console.log("ini adalah tipe data number: " + number);
+
+// Tipe data string
+var string = "Halo saya sedang belajar Javascript";
+console.log("ini adalah tipe data string: " + string);
+
+// Tipe data boolean
+var boolean = true;
+console.log("ini adalah tipe data boolean: " + boolean);
+
+// Tipe data null & undifined
+var kosong = null;
+var tidakterdefinisi;
+console.log("ini adalah tipe data null: " + kosong);
+console.log("ini adalah tipe data unddifined: " + tidakterdefinisi);
+
+// Tipe data array
+var array = ["apel", "mangga", "pisang"];
+console.log("ini adalah tipe data array: " + array);
